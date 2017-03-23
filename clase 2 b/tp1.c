8b2fbc3c84faa2633eae02493a90be81934c28df
cristian4891 <cristianecossio@gmail.com>
2017-03-22 22:03:03 -0300

    Add files via upload

---
+++ clase 2 b/tp1.c
@@ -0,0 +1,28 @@
+{
+ float numero1;
+ float numero2;
+ float total1;
+
+ printf("Ingrese un numero: ");
+ scanf("%f",&numero1);
+ printf("Ingrese el divisor: ");
+ scanf("%f",&numero2);
+ total1=division(numero1,numero2);
+ printf("El resultado es: %f",total1);
+ return 0;
+
+}
+
+{
+    float total1_1;
+
+    if(numero2_2==0)
+        {
+            printf("Error, el divisor no puede ser 0, reingrese el numero: ");
+            scanf("%f",&numero2_2);
+        }
+    else
+        {
+            total1_1=numero1_1/numero2_2;
+        }
+}
---
+++ clase 2 b/tp1.h
@@ -0,0 +1,2 @@
+float division(float numero1_1,float numero2_2)
+float division(float numero1_1,float numero2_2)
